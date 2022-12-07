aaa3ea1bde6917bac203bd532f7b2b9e06b12705
Bartek Szczepanski <bartek.szczepanski@nightwoods.io>
2022-12-07 14:26:44 +0100

    add create2 form

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,182 +1,25 @@
-import { LoaderFunction } from "@remix-run/cloudflare"
-import { useLoaderData, Link } from "@remix-run/react"
-import { CoupleWithSpouses } from "~/db/couples-db.server"
+import AddIcon from "@mui/icons-material/Add"
+import ClearIcon from "@mui/icons-material/Clear"
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+} from "@mui/material"
+import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
-import ClearIcon from "@mui/icons-material/Clear"
-import AddIcon from "@mui/icons-material/Add"
-import Box from "@mui/material/Box"
-import DataTable from "../components/datatable"
+import { LoaderFunction } from "@remix-run/cloudflare"
+import { Link, useLoaderData } from "@remix-run/react"
 import { useState } from "react"
-import { randomId } from "@mui/x-data-grid-generator"
-import {
-  FormControl,
-  FormLabel,
-  FormGroup,
-  FormControlLabel,
-  Checkbox,
-} from "@mui/material"
+import { CoupleWithSpouses } from "~/db/couples-db.server"
+import { readCouples } from "~/db/in-memory-db"
+import DataTable from "../components/datatable"
 
 export const loader: LoaderFunction = async (): Promise<LoaderData> => {
   return {
-    couples: [
-      {
-        id: "2-1-2005.01-15",
-        city: "Wrocław",
-        group: "A",
-        postalCode: "50-123",
-        weddingYear: 2000,
-        wifeId: "1234",
-        husbandId: "5678",
-        invitedById: null,
-        wife: {
-          id: randomId(),
-          email: "email@email-wife.com",
-          lastName: "Kowalska",
-          firstName: "Anna",
-          birthYear: 1980,
-          phoneNumber: "123456789",
-        },
-        husband: {
-          id: randomId(),
-          email: "husband@email.com",
-          lastName: "Kowalski",
-          firstName: "Jan",
-          birthYear: 1975,
-          phoneNumber: "987654321",
-        },
-      },
-      {
-        id: "2-1-2006.01-15",
-        city: "Wrocław",
-        group: "B",
-        postalCode: "50-123",
-        weddingYear: 2001,
-        wifeId: "12340",
-        husbandId: "56780",
-        invitedById: null,
-        wife: {
-          id: randomId(),
-          email: "email2@email-wife.com",
-          lastName: "Nowak",
-          firstName: "Grażyna",
-          birthYear: 1981,
-          phoneNumber: "123456780",
-        },
-        husband: {
-          id: randomId(),
-          email: "husband2@email.com",
-          lastName: "Nowak",
-          firstName: "Janusz",
-          birthYear: 1976,
-          phoneNumber: "987654320",
-        },
-      },
-      {
-        id: "1-2-2010.02-13",
-        city: "Warszawa",
-        group: "S",
-        postalCode: "00-001",
-        weddingYear: 2003,
-        wifeId: "92340",
-        husbandId: "96780",
-        invitedById: null,
-        wife: {
-          id: randomId(),
-          email: "email32@email-wife.com",
-          lastName: "Lewandowska",
-          firstName: "Barbara",
-          birthYear: 1965,
-          phoneNumber: "923456780",
-        },
-        husband: {
-          id: randomId(),
-          email: "husband23@email.com",
-          lastName: "Lewandowski",
-          firstName: "Andrzej",
-          birthYear: 1966,
-          phoneNumber: "187654320",
-        },
-      },
-      {
-        id: "2-2-2011.01-14",
-        city: "Warszawa",
-        group: "C",
-        postalCode: "00-001",
-        weddingYear: 2003,
-        wifeId: "921340",
-        husbandId: "961780",
-        invitedById: null,
-        wife: {
-          id: randomId(),
-          email: "email312@email-wife.com",
-          lastName: "Leszczyńska",
-          firstName: "Oliwia",
-          birthYear: 1991,
-          phoneNumber: "913456780",
-        },
-        husband: {
-          id: randomId(),
-          email: "husband213@email.com",
-          lastName: "Leszczyński",
-          firstName: "Dawid",
-          birthYear: 1936,
-          phoneNumber: "117654320",
-        },
-      },
-      {
-        id: "1-2-2010.02-14",
-        city: "Wrocław",
-        group: "X",
-        postalCode: "00-001",
-        weddingYear: 2002,
-        wifeId: "292340",
-        husbandId: "296780",
-        invitedById: null,
-        wife: {
-          id: randomId(),
-          email: "email232@email-wife.com",
-          lastName: "Kaczyński",
-          firstName: "Kot",
-          birthYear: 2002,
-          phoneNumber: "323456780",
-        },
-        husband: {
-          id: randomId(),
-          email: "husband323@email.com",
-          lastName: "Kaczyński",
-          firstName: "Jarosław",
-          birthYear: 1959,
-          phoneNumber: "387654320",
-        },
-      },
-      {
-        id: "1-2-2010.02-15",
-        city: "Wrocław",
-        group: "D",
-        postalCode: "50-123",
-        weddingYear: 2000,
-        wifeId: "42340",
-        husbandId: "46780",
-        invitedById: null,
-        wife: {
-          id: randomId(),
-          email: "email342@email-wife.com",
-          lastName: "Dziuba",
-          firstName: "Sara",
-          birthYear: 1993,
-          phoneNumber: "423456780",
-        },
-        husband: {
-          id: randomId(),
-          email: "husband243@email.com",
-          lastName: "Dziuba",
-          firstName: "Michał",
-          birthYear: 1994,
-          phoneNumber: "487654320",
-        },
-      },
-    ],
+    couples: readCouples(),
   }
 }
 
