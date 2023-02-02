739a2485da2639577d48edab619ed053728fcae7
Bartek Szczepanski <bartek.szczepanski@nightwoods.io>
2023-02-02 15:06:09 +0100

    add skeleton for couples import backend

--- app/routes/csvimporter.tsx
+++ app/routes/csvimporter.tsx
@@ -1,16 +1,16 @@
 import { Button } from "@mui/material"
 import { randomId } from "@mui/x-data-grid-generator"
+import type { ActionArgs } from "@remix-run/node"
 import {
-  ActionArgs,
   redirect,
   unstable_composeUploadHandlers,
-  unstable_createFileUploadHandler,
   unstable_createMemoryUploadHandler,
   unstable_parseMultipartFormData,
 } from "@remix-run/node"
 import { Form } from "@remix-run/react"
-import { db } from "~/db/db.server"
 import Papa from "papaparse"
+import { db } from "~/db/db.server"
+import type { Prisma } from "@prisma/client"
 
 export const action = async ({ request }: ActionArgs) => {
   const uploadHandler = unstable_composeUploadHandlers(
@@ -28,55 +28,109 @@ export const action = async ({ request }: ActionArgs) => {
   // console.log(parsedData)
   console.log(parsedData.data[0]["nazwisko męża"])
 
-  await db.couple.create({
-    data: {
-      comments: String(formData.get("Grupa")),
-      coupleId: randomId(),
-      almaEvent: {
-        connect: {
-          id: "030b2a79-bd49-41af-9081-837d8e817e9d",
-        },
-      },
-      organizationUnit: {
-        connect: {
-          id: 1,
-        },
-      },
-      attendanceNumber: 17,
-      city: "data.miejscowość",
-      group: "A",
-      postalCode: "data.kodpocztowy",
-      weddingYear: 2002,
-      invitedBy: {
-        connect: {
-          id: "5a926019-9202-461e-9fd1-934db1f19051",
-        },
-      },
-      wife: {
-        create: {
-          vocative: "data.wołaczmąż",
-          church: 2,
-          email: "data.mailmąż",
-          lastName: "data.nazwiskomęża",
-          firstName: String(data),
-          phoneNumber: "data.telefonmąż",
-          birthYear: 2001,
-        },
-      },
-      husband: {
-        create: {
-          vocative: "data.wołaczżona",
-          church: 3,
-          email: "data.mailżona",
-          lastName: "data.nazwiskożony",
-          firstName: "data.imiężona",
-          phoneNumber: "data.telefonżona",
-          birthYear: 1999,
-        },
-      },
-    },
-  })
+  const couples: Prisma.CoupleCreateManyInput[] = []
 
+  for (const couple of parsedData.data) {
+    // check if alma event exists (findMany) if not create it
+    /*
+    {
+      'nazwisko męża': 'Kowalczyk',
+      'imię mąż': 'Asdkjasddasdsapko',
+      'wołacz mąż': 'Saddas',
+      'nazwisko żony': 'Kowsaddsaalczyk',
+      'imię żona': 'Janina',
+      'wołacz żona': 'Asddas',
+      'telefon mąż': '4102984',
+      'telefon żona': '574190',
+      'Kod Pocztowy': '90909',
+      'Miejscowość': 'Białystok',
+      Grupa: 'A',
+      'zapraszający': 'Kowalski Bogdan Bożena',
+      'zapraszający nr bazy': '2',
+      'nr bazy': '2',
+      'nr Almy': '2',
+      'kiedy na Almie': '1997.11',
+      nr: '18',
+      'rok ur mąż': '1965',
+      'rok ur żona': '1990',
+      'rok ślubu': '2008',
+      'mail mąż': 'jan@onet.pl',
+      'mail żona': 'ania@wp.pl',
+      'kościół mąż': '2',
+      'kościół żona': '2',
+      UWAGI: 'Bardzo ważne uwagi sprawdzamy jak się wyświetlają'
+    }
+     */
+    const almaEvent = await db.almaEvent.findFirst({
+      where: {
+        year: couple["kiedy na Almie"].split(".")[0],
+        month: couple["kiedy na Almie"].split(".")[1],
+      },
+    })
+
+    const husband = await db.person.create({
+      data: {
+        //...
+      },
+    })
+
+    couples.push({
+      city: couple["Miejscowość"],
+      group: couple["Grupa"],
+      almaEventId: "", // create or find this ID!!
+      husbandId: husband.id,
+    })
+  }
+
+  // await db.couple.create({
+  //   data: {
+  //     comments: String(formData.get("Grupa")),
+  //     coupleId: randomId(),
+  //     almaEvent: {
+  //       connect: {
+  //         id: "030b2a79-bd49-41af-9081-837d8e817e9d",
+  //       },
+  //     },
+  //     organizationUnit: {
+  //       connect: {
+  //         id: 1,
+  //       },
+  //     },
+  //     attendanceNumber: 17,
+  //     city: "data.miejscowość",
+  //     group: "A",
+  //     postalCode: "data.kodpocztowy",
+  //     weddingYear: 2002,
+  //     invitedBy: {
+  //       connect: {
+  //         id: "5a926019-9202-461e-9fd1-934db1f19051",
+  //       },
+  //     },
+  //     wife: {
+  //       create: {
+  //         vocative: "data.wołaczmąż",
+  //         church: 2,
+  //         email: "data.mailmąż",
+  //         lastName: "data.nazwiskomęża",
+  //         firstName: String(data),
+  //         phoneNumber: "data.telefonmąż",
+  //         birthYear: 2001,
+  //       },
+  //     },
+  //     husband: {
+  //       create: {
+  //         vocative: "data.wołaczżona",
+  //         church: 3,
+  //         email: "data.mailżona",
+  //         lastName: "data.nazwiskożony",
+  //         firstName: "data.imiężona",
+  //         phoneNumber: "data.telefonżona",
+  //         birthYear: 1999,
+  //       },
+  //     },
+  //   },
+  // })
+  //
   return redirect("/couples")
 }
 
