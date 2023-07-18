4247800b0924ef8cc99ce48cc2f1554431005460
Karol Stawowski <karol.stawowski@nightwoods.io>
2023-07-18 12:02:33 +0200

    chore: bump remix version

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node"
+import type { V2_MetaFunction } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -14,11 +14,9 @@ export function links() {
   return [{ rel: "stylesheet", href: styles }]
 }
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Alma",
-  viewport: "width=device-width,initial-scale=1",
-})
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Alma" }]
+}
 
 export default function App() {
   return (
