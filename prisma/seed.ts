d361b684cb15a070974a4c6f9b827824fae54bf7
Karol Stawowski <karol.stawowski@nightwoods.io>
2023-07-24 16:59:32 +0200

    feat: add fly deployment

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client"
-import { randomUUID } from "crypto"
 import { hash } from "../app/db/hash.server"
 
 const prisma = new PrismaClient()
@@ -12,7 +11,7 @@ async function main() {
     },
   })
 
-  const wroclaw = await prisma.organizationUnit.create({
+  await prisma.organizationUnit.create({
     data: {
       id: 2,
       name: "Wrocław",
@@ -31,52 +30,6 @@ async function main() {
       username: "Admin",
       passwordHash: await hash("Admin12!", 10),
       organizationUnitId: 1,
-    },
-  })
-
-  const alma = await prisma.almaEvent.create({
-    data: {
-      year: 2022,
-      month: 3,
-      organizationUnitId: 2,
-    },
-  })
-
-  await prisma.couple.create({
-    data: {
-      coupleId: "2-1-2022.03-40",
-      comments: "",
-      almaEvent: { connect: { id: alma.id } },
-      organizationUnit: { connect: { id: wroclaw.id } },
-      city: "Wrocław",
-      group: "A",
-      postalCode: "50-123",
-      weddingYear: 2000,
-      attendanceNumber: 40,
-      wife: {
-        create: {
-          church: 1,
-          vocative: "Anno",
-          id: randomUUID(),
-          email: "email@email-wife.com",
-          lastName: "Kowalska",
-          firstName: "Anna",
-          birthYear: 1980,
-          phoneNumber: "123456789",
-        },
-      },
-      husband: {
-        create: {
-          church: 1,
-          vocative: "Anno",
-          id: randomUUID(),
-          email: "husband@email.com",
-          lastName: "Kowalski",
-          firstName: "Jan",
-          birthYear: 1975,
-          phoneNumber: "987654321",
-        },
-      },
     },
   })
 }
